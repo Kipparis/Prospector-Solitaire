fb22d8905d5c8ee196e6b1a2ad12cf8afe1495d1
Un <keepintine@gmail.com>
2018-07-06 01:09:17 +0300

    Work with sprites

---
+++ Prospector/Assets/__Scripts/Card.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour {
+    // Это реализуем позже
+}
+
+[System.Serializable]
+public class Decorator {    // Типо знак
+    // Класс содержит сведения о любом знаке или указатель
+    public string type; // Для указатель тип pip
+    public Vector3 loc; // Позиция спрайта на карте
+    public bool flip = false;   // Нужно ли перевернуть спрайт вертикально
+    public float scale = 1f;    // Увеличение спрайта
+}
+
+[System.Serializable]
+public class CardDefinition {
+    // Содержит инфу о любом ранге из карт
+    public string face; // Спрайт чтобы показать лицевую часть (>10)
+    public int rank;    // Ранг
+    public List<Decorator> pips = new List<Decorator>();    // указатели
+    // Так как знаки (маленькие значки под рангом) одинаковы во всех картах
+    // указатели появляются только на нумерованных картах
+}
